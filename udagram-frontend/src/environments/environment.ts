7486f6076595e1d4d12261447b4e51f9f220cb0f
Peter Pajor <saxumvermes@protonmail.com>
2023-04-09 01:01:07 +0200

    revert back due to improper property access

--- udagram-frontend/src/environments/environment.ts
+++ udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://a61b0504d36734bf4960b2fc2b4120ad-1483068635.us-east-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
