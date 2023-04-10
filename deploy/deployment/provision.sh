0228c15b3db227ea2e88b24bd5c94f67d9232f4c
Peter Pajor <saxumvermes@protonmail.com>
2023-04-10 20:47:12 +0200

    update public url of reverse proxy and deployment files

--- deploy/deployment/provision.sh
+++ deploy/deployment/provision.sh
@@ -4,5 +4,5 @@ here=$(dirname ${BASH_SOURCE[0]})
 
 kubectl apply -f ${here}/udagram-api-feed.deployment.yaml
 kubectl apply -f ${here}/udagram-api-user.deployment.yaml
-kubectl apply -f ${here}/udagram-frontend.deployment.yaml
 kubectl apply -f ${here}/udagram-reverse-proxy.deployment.yaml
+kubectl apply -f ${here}/udagram-frontend.deployment.yaml
--- udagram-frontend/src/environments/environment.ts
+++ udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://a61b0504d36734bf4960b2fc2b4120ad-1483068635.us-east-1.elb.amazonaws.com:8080/api/v0'
+  apiHost: 'http://a60aa6814eaaf410dba0ee7f681744ad-1257487998.us-east-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
